ab616a4c61cebefc72ad4f26f744158b28ceaec2
Arturo-Castro <castro.ah91@gmail.com>
2021-10-11 22:45:37 -0300

    adds code refactoring

--- main.js
+++ main.js
@@ -3,19 +3,15 @@ let $cardChosenBack = [];
 let $cardChosenFront = [];
 const $cards = document.querySelectorAll('.card-front');
 let $cardsUrl = [
-"https://i.pinimg.com/originals/8c/74/37/8c7437379cdcd940a4229f28102bdc80.jpg",
-"https://i.pinimg.com/originals/8c/74/37/8c7437379cdcd940a4229f28102bdc80.jpg",
-"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRaErfScJAGPLs517jilnu9sjXcoX1GG7CFBx8dZ4hKNrO00x2T48Sm65YEpOjY2HfPOdE&usqp=CAU", 
-"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRaErfScJAGPLs517jilnu9sjXcoX1GG7CFBx8dZ4hKNrO00x2T48Sm65YEpOjY2HfPOdE&usqp=CAU",
-"https://www.teahub.io/photos/full/105-1052004_iron-man-wallpaper-art.jpg",
-"https://www.teahub.io/photos/full/105-1052004_iron-man-wallpaper-art.jpg", 
-"https://tooys.mx/pub/media/catalog/product/cache/96210167b6a992dbdbaba9ff58d054c8/m/a/magneto_marvel_02_1.jpg",
-"https://tooys.mx/pub/media/catalog/product/cache/96210167b6a992dbdbaba9ff58d054c8/m/a/magneto_marvel_02_1.jpg",
-"https://i.pinimg.com/originals/3e/16/bf/3e16bff253abb380070095370ef15ae5.jpg",
-"https://i.pinimg.com/originals/3e/16/bf/3e16bff253abb380070095370ef15ae5.jpg",
-"https://images.hdqwalls.com/download/wolverine-comic-book-art-4k-mk-1125x2436.jpg",
-"https://images.hdqwalls.com/download/wolverine-comic-book-art-4k-mk-1125x2436.jpg"
+"./img/Apocalypse.jpg",
+"./img/Ironman.jpg", 
+"./img/Magneto.jpg", 
+"./img/Spiderman.jpg",
+"./img/Venom.jpg",
+"./img/Wolverine.jpg"
 ]
+$cardsUrl = $cardsUrl.concat($cardsUrl);
+const winningCondition = $cardsUrl.length/2;
 
 function blockPlayerInput(){
     $cards.forEach(function($card){
@@ -39,14 +35,13 @@ function analyzeRound(e){
         if($cardChosenBack[0].src === $cardChosenBack[1].src){
             counter++;
             setTimeout(function(){
-                $cardChosenBack[0].classList.add('hidden');
-                $cardChosenBack[1].classList.add('hidden');
-                $cardChosenFront[0].classList.add('hidden');
-                $cardChosenFront[1].classList.add('hidden');
+                [$cardChosenBack[0], $cardChosenBack[1], $cardChosenFront[0], $cardChosenFront[1]].forEach(function($card){
+                    $card.classList.add('hidden');
+                })
                 $cardChosenBack = [];
                 $cardChosenFront = [];
                 unblockPlayerInput();
-                if(counter === 6){
+                if(winningCondition === counter){
                     document.querySelector('h1').textContent = 'You win!, restarting...';
                     setTimeout(function(){
                         window.location.reload();
